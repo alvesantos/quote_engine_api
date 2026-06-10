b8b17aaeccbce43860eca705c8b50e3e9ce3a76c
ebagabee <alvesantos.gh@gmail.com>
2026-06-10 18:27:54 -0300

    chore: remove hello to test the endpoint

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,5 @@ use App\Http\Controllers\HelloController;
 use App\Http\Controllers\QuoteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('hello', [HelloController::class, 'hello']);
-
 Route::post('quotes', [QuoteController::class, 'store']);
 Route::get('/quotes', [QuoteController::class, 'index']);

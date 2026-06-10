57a9fea2125e3a6c0d93d938366565149d1c1285
ebagabee <alvesantos.gh@gmail.com>
2026-06-10 18:26:08 -0300

    feat: implement provider bind and routes to endpoint

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('hello', [HelloController::class, 'hello']);
 
 Route::post('quotes', [QuoteController::class, 'store']);
+Route::get('/quotes', [QuoteController::class, 'index']);

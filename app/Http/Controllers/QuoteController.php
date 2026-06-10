2070526fdd0b50ce96d121611ddad1b2c7a0a626
ebagabee <alvesantos.gh@gmail.com>
2026-06-10 18:24:26 -0300

    feat: add method do list all quotes

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\QuoteRequest;
 use App\Services\QuoteService;
+use Illuminate\Http\JsonResponse;
 
 class QuoteController extends Controller
 {
@@ -11,7 +12,13 @@ class QuoteController extends Controller
 
     public function store(QuoteRequest $request)
     {
-        $result = $this->service->calculate($request->toArray());
+        $result = $this->service->calculateAndSave($request->toArray());
         return response()->json($result);
+    }
+
+    public function index()
+    {
+        $quotes = $this->service->listAll();
+        return response()->json($quotes);
     }
 }
